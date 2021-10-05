ee821f735da3d07b20be0a8c3874b112ccb5e399
itchyny <itchyny@hatena.ne.jp>
2021-10-06 07:21:35 +0900

    implement all metadata APIs

--- src/metadata.rs
+++ src/metadata.rs
@@ -20,8 +20,8 @@ struct ListMetadataResponse {
 impl client::Client {
     /// Retrieves a host metadata.
     ///
-    /// See https://mackerel.io/api-docs/entry/metadata#get.
-    pub async fn get_metadata(
+    /// See https://mackerel.io/api-docs/entry/metadata#hostget.
+    pub async fn get_host_metadata(
         &self,
         host_id: HostId,
         namespace: &str,
@@ -36,10 +36,10 @@ impl client::Client {
         .await
     }
 
-    /// Creates/Updatates a host metadata.
+    /// Creates/Updates a host metadata.
     ///
-    /// See https://mackerel.io/api-docs/entry/metadata#put.
-    pub async fn put_metadata(
+    /// See https://mackerel.io/api-docs/entry/metadata#hostput.
+    pub async fn put_host_metadata(
         &self,
         host_id: HostId,
         namespace: &str,
@@ -57,8 +57,8 @@ impl client::Client {
 
     /// Deletes a host metadata.
     ///
-    /// See https://mackerel.io/api-docs/entry/metadata#delete.
-    pub async fn delete_metadata(&self, host_id: HostId, namespace: &str) -> Result<()> {
+    /// See https://mackerel.io/api-docs/entry/metadata#hostdelete.
+    pub async fn delete_host_metadata(&self, host_id: HostId, namespace: &str) -> Result<()> {
         self.request(
             Method::DELETE,
             format!("/api/v0/hosts/{}/metadata/{}", host_id, namespace),
@@ -71,11 +71,168 @@ impl client::Client {
 
     /// Lists host metadata.
     ///
-    /// See https://mackerel.io/api-docs/entry/metadata#list.
-    pub async fn list_metadata(&self, host_id: HostId) -> Result<Vec<Metadata>> {
+    /// See https://mackerel.io/api-docs/entry/metadata#hostlist.
+    pub async fn list_host_metadata(&self, host_id: HostId) -> Result<Vec<Metadata>> {
         self.request(
             Method::GET,
             format!("/api/v0/hosts/{}/metadata", host_id),
+            vec![],
+            client::empty_body(),
+            |res: ListMetadataResponse| res.metadata,
+        )
+        .await
+    }
+
+    /// Retrieves a service metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#serviceget.
+    pub async fn get_service_metadata(
+        &self,
+        service_name: String,
+        namespace: &str,
+    ) -> Result<serde_json::Value> {
+        self.request(
+            Method::GET,
+            format!("/api/v0/services/{}/metadata/{}", service_name, namespace),
+            vec![],
+            client::empty_body(),
+            |res| res,
+        )
+        .await
+    }
+
+    /// Creates/Updates a service metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#serviceput.
+    pub async fn put_service_metadata(
+        &self,
+        service_name: String,
+        namespace: &str,
+        metadata: serde_json::Value,
+    ) -> Result<()> {
+        self.request(
+            Method::PUT,
+            format!("/api/v0/services/{}/metadata/{}", service_name, namespace),
+            vec![],
+            Some(metadata),
+            |_: serde_json::Value| (),
+        )
+        .await
+    }
+
+    /// Deletes a service metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#servicedelete.
+    pub async fn delete_service_metadata(
+        &self,
+        service_name: String,
+        namespace: &str,
+    ) -> Result<()> {
+        self.request(
+            Method::DELETE,
+            format!("/api/v0/services/{}/metadata/{}", service_name, namespace),
+            vec![],
+            client::empty_body(),
+            |_: serde_json::Value| (),
+        )
+        .await
+    }
+
+    /// Lists service metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#servicelist.
+    pub async fn list_service_metadata(&self, service_name: String) -> Result<Vec<Metadata>> {
+        self.request(
+            Method::GET,
+            format!("/api/v0/services/{}/metadata", service_name),
+            vec![],
+            client::empty_body(),
+            |res: ListMetadataResponse| res.metadata,
+        )
+        .await
+    }
+
+    /// Retrieves a role metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#roleget.
+    pub async fn get_role_metadata(
+        &self,
+        service_name: String,
+        role_name: String,
+        namespace: &str,
+    ) -> Result<serde_json::Value> {
+        self.request(
+            Method::GET,
+            format!(
+                "/api/v0/services/{}/roles/{}/metadata/{}",
+                service_name, role_name, namespace
+            ),
+            vec![],
+            client::empty_body(),
+            |res| res,
+        )
+        .await
+    }
+
+    /// Creates/Updates a role metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#roleput.
+    pub async fn put_role_metadata(
+        &self,
+        service_name: String,
+        role_name: String,
+        namespace: &str,
+        metadata: serde_json::Value,
+    ) -> Result<()> {
+        self.request(
+            Method::PUT,
+            format!(
+                "/api/v0/services/{}/roles/{}/metadata/{}",
+                service_name, role_name, namespace
+            ),
+            vec![],
+            Some(metadata),
+            |_: serde_json::Value| (),
+        )
+        .await
+    }
+
+    /// Deletes a role metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#roledelete.
+    pub async fn delete_role_metadata(
+        &self,
+        service_name: String,
+        role_name: String,
+        namespace: &str,
+    ) -> Result<()> {
+        self.request(
+            Method::DELETE,
+            format!(
+                "/api/v0/services/{}/roles/{}/metadata/{}",
+                service_name, role_name, namespace
+            ),
+            vec![],
+            client::empty_body(),
+            |_: serde_json::Value| (),
+        )
+        .await
+    }
+
+    /// Lists role metadata.
+    ///
+    /// See https://mackerel.io/api-docs/entry/metadata#rolelist.
+    pub async fn list_role_metadata(
+        &self,
+        service_name: String,
+        role_name: String,
+    ) -> Result<Vec<Metadata>> {
+        self.request(
+            Method::GET,
+            format!(
+                "/api/v0/services/{}/roles/{}/metadata",
+                service_name, role_name,
+            ),
             vec![],
             client::empty_body(),
             |res: ListMetadataResponse| res.metadata,
