2bada19c1a21f0cc56db248c4a3a4331675675a4
itchyny <itchyny@cybozu.co.jp>
2023-11-01 09:59:02 +0900

    allow comparing names of incomparable types

--- src/name.rs
+++ src/name.rs
@@ -2,7 +2,7 @@ use fixedstr::str64;
 use serde_with::{DeserializeFromStr, SerializeDisplay};
 use std::marker::PhantomData;
 
-#[derive(PartialEq, Eq, SerializeDisplay, DeserializeFromStr)]
+#[derive(SerializeDisplay, DeserializeFromStr)]
 pub struct Name<T>(str64, PhantomData<T>);
 
 impl<T> Name<T> {
@@ -18,6 +18,14 @@ impl<T> Clone for Name<T> {
         *self
     }
 }
+
+impl<T> PartialEq for Name<T> {
+    fn eq(&self, other: &Self) -> bool {
+        *self.0 == *other.0
+    }
+}
+
+impl<T> Eq for Name<T> {}
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct ParseNameError(String);
@@ -83,5 +91,31 @@ impl<T> std::fmt::Debug for Name<T> {
 impl<T> std::hash::Hash for Name<T> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         self.0.hash(state);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+    use std::collections::{HashMap, HashSet};
+
+    struct Value {
+        #[allow(dead_code)]
+        x: String,
+    }
+
+    #[test]
+    fn name() {
+        let name = Name::<Value>::from("ExampleName");
+        assert_eq!(name, "ExampleName".into());
+        assert_eq!(HashMap::from([(name, 1)]).get(&name), Some(&1));
+        assert_eq!(HashSet::from([name]).iter().next(), Some(&name));
+        assert_eq!(name.to_string(), "ExampleName");
+        assert_eq!(format!("{}", name), "ExampleName");
+        assert_eq!(format!("{:?}", name), r#""ExampleName""#);
+        assert_eq!(name, "ExampleName".parse().unwrap());
+        assert_eq!(name, serde_json::from_value(json!("ExampleName")).unwrap());
+        assert_eq!(serde_json::to_value(name).unwrap(), json!("ExampleName"));
     }
 }
