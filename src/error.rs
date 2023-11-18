b91a9360c902f29ad64979c81dcec138b71f0d1b
itchyny <itchyny@cybozu.co.jp>
2023-11-18 23:43:26 +0900

    derive PartialEq for Error type using derivative crate

--- src/error.rs
+++ src/error.rs
@@ -1,29 +1,20 @@
+use derivative::Derivative;
 use http::StatusCode;
 use thiserror::Error;
 
 /// Error represents the error type of the library.
-#[derive(Debug, Error)]
+#[derive(Debug, Derivative, Error)]
+#[derivative(PartialEq)]
 pub enum Error {
-    #[error("status:{0}, message:{1}")]
+    #[error("status_code:{0}, message:{1}")]
     ApiError(StatusCode, String),
 
     #[error(transparent)]
-    RequestError(#[from] reqwest::Error),
-}
-
-impl PartialEq for Error {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (
-                Self::ApiError(status_code1, error_message1),
-                Self::ApiError(status_code2, error_message2),
-            ) => status_code1 == status_code2 && error_message1 == error_message2,
-            (Self::RequestError(err1), Self::RequestError(err2)) => {
-                err1.to_string() == err2.to_string()
-            }
-            _ => false,
-        }
-    }
+    RequestError(
+        #[from]
+        #[derivative(PartialEq = "ignore")]
+        reqwest::Error,
+    ),
 }
 
 /// Result alias where the error type is [`crate::Error`].
